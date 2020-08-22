5201eaa39bb981bf9ce5a5a82e0817fe45b8d1ac
ryougi1 <gulong.james@gmail.com>
2020-08-23 00:42:28 +0200

    NETLIFY: omitted loader

--- src/components/index.js
+++ src/components/index.js
@@ -1,4 +1,4 @@
-export { default as Loader } from './loader';
+// export { default as Loader } from './loader';
 export { default as Hero } from './sections/hero';
 export { default as About } from './sections/about';
 export { default as Jobs } from './sections/jobs';
--- src/components/layout.js
+++ src/components/layout.js
@@ -1,7 +1,8 @@
 import React, { useState, useEffect } from 'react';
 import PropTypes from 'prop-types';
 import styled from 'styled-components';
-import { Loader, SidebarLeft, SidebarRight } from '@components';
+// import { Loader, SidebarLeft, SidebarRight } from '@components';
+import { SidebarLeft, SidebarRight } from '@components';
 import { GlobalStyle } from '@styles';
 
 const StyledContent = styled.div`
