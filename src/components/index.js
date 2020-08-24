67ec6144227e9e9221d081a198c0376439ffab1f
ryougi1 <gulong.james@gmail.com>
2020-08-24 15:28:53 +0200

    DEP: omitt projects for deploy

--- src/components/index.js
+++ src/components/index.js
@@ -2,7 +2,7 @@ export { default as Loader } from './loader';
 export { default as Hero } from './sections/hero';
 export { default as About } from './sections/about';
 export { default as Jobs } from './sections/jobs';
-export { default as Projects } from './sections/projects';
+// export { default as Projects } from './sections/projects';
 export { default as Layout } from './layout';
 export { default as SidebarLeft } from './sidebarLeft';
 export { default as SidebarRight } from './sidebarRight';
